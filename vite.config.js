d35b22c3f9691f2cfc8070c9e4f9fdd7d5fd595e
proheuros-creator <proheuros@gmail.com>
2025-12-19 01:05:51 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 배포 시 경로 문제를 방지하기 위해 base 설정을 추가할 수 있습니다.
+  base: './',
 })
